114e05bccfad4a65c0e7b5b0c65eb6667cfa8038
cgslav <l.dudzic@gmail.com>
2024-02-25 10:11:58 +0100

    acorn update

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,5 +1,24 @@
 import domReady from '@roots/sage/client/dom-ready';
+import { createApp, ref } from 'vue'
 
+
+
+
+/**
+ * Application entrypoint
+ */
 domReady(async () => {
-  
+  // ...
+  createApp({
+    setup() {
+      return {
+        count: ref(0)
+      }
+    }
+  }).mount('#vue-publikacje')
 });
+
+/**
+ * @see {@link https://webpack.js.org/api/hot-module-replacement/}
+ */
+if (import.meta.webpackHot) import.meta.webpackHot.accept(console.error);
