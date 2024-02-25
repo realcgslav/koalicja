cbd3c2fb7206122b42f977e72617c8c8308bf670
cgslav <l.dudzic@gmail.com>
2024-02-25 01:11:15 +0100

    dodany vue

--- bud.config.js
+++ bud.config.js
@@ -16,6 +16,7 @@ export default async (app) => {
   app
     .entry('app', ['@scripts/app', '@styles/app'])
     .entry('editor', ['@scripts/editor', '@styles/editor'])
+    .entry('vue-publikacje', '@scripts/vue-publikacje')
     .assets(['images']);
 
   /**
---
+++ resources/scripts/vue-publikacje.js
@@ -0,0 +1,10 @@
+import { createApp, ref } from 'vue'
+
+createApp({
+  setup() {
+    return {
+      count: ref(0)
+    }
+  }
+}).mount('#vue-publikacje')
+
