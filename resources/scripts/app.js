b15c20d8249b9c0c924fe98d124fe0b0a920be72
cgslav <l.dudzic@gmail.com>
2024-05-12 17:48:45 +0200

    remove swiper

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,5 +1,4 @@
 import domReady from '@roots/sage/client/dom-ready';
-import Swiper, { Navigation, Pagination } from 'swiper';
 
 domReady(async () => {
 
@@ -9,20 +8,8 @@ domReady(async () => {
   hamburger.addEventListener('click', function () {
     nav.classList.toggle('nav-mobile-visible');
   });
-  
-  // Inicjalizacja Swipera
-  const swiper = new Swiper('.swiper', {
-    // Parametry Swipera
-    modules: [Navigation, Pagination],
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination", 
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+
+
 });
 
 if (import.meta.webpackHot) import.meta.webpackHot.accept(console.error);
